a140d0a830689d0e246866edfb4c07b65f3ec9a2
Vu Tran <phongvu.811@gmail.com>
2017-04-13 23:31:31 +0700

    update version

--- Menu.php
+++ Menu.php
@@ -2,15 +2,6 @@
 
 namespace Piwik\Plugins\QualityAssurance;
 
-//use Piwik\Menu\MenuTop;
-//use Piwik\Piwik;
+class Menu extends \Piwik\Plugin\Menu {
 
-class Menu extends \Piwik\Plugin\Menu
-{
-//    public function configureTopMenu(MenuTop $menu)
-//    {
-//        if (!Piwik::hasUserSuperUserAccess()) return;
-//        $tooltip = Piwik::translate('QualityAssurance_QualityAssurance');
-//        $menu->addItem(Piwik::translate('QualityAssurance_QualityAssurance'), '', $this->urlForAction('index'), 2, $tooltip);
-//    }
 }
